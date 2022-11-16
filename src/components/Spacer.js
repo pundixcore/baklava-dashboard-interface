15de2d5424ec77463cf7c00f0ea3fd1151ae812c
Issei Kumagai <48951453+IsseiBlockchain@users.noreply.github.com>
2022-11-16 17:04:28 +0800

    reformatted

--- src/components/Spacer.js
+++ src/components/Spacer.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import { useTheme } from '@mui/material';
+import React from "react";
+import Box from "@mui/material/Box";
+import { useTheme } from "@mui/material";
 
 const Spacer = ({ sx = [] }) => {
-    const theme = useTheme();
+  const theme = useTheme();
 
-    return (
-        <Box
-            sx={[
-                {
-                    backgroundColor: theme.palette.background.default
-                },
-                ...[sx]
-            ]}
-        ></Box>
-    );
+  return (
+    <Box
+      sx={[
+        {
+          backgroundColor: theme.palette.background.default,
+        },
+        ...[sx],
+      ]}
+    ></Box>
+  );
 };
 
 export default Spacer;
